c398f237d3ffc246833ad641f8fd9ec4c1581e1e
wilco <wilco@be8bedfb-b93a-dc11-ad5b-00609709f61e>
2007-09-04 12:55:00 +0000

    added the calculation of time to stop in Link::enter_veh
    git-svn-id: svn+ssh://ruttval.infra.kth.se/svn/mezzo@64 be8bedfb-b93a-dc11-ad5b-00609709f61e

--- branches/busmezzo/mezzo_lib/src/link.cpp
+++ branches/busmezzo/mezzo_lib/src/link.cpp
@@ -331,13 +331,15 @@ bool Link::enter_veh(Vehicle* veh, double time)
 		}
 	}
 // add here the BUSSTOP FUNCTIONS
-	
+//#define _BUSES
 #ifdef _BUSES
 	if (typeid (veh) == typeid (Bus))
 	{
 		// Calc time to stop
 		Bus* bus =  (Bus*)(veh); // so we can do Bus operations
 		Visit_stop* next_stop = (bus->get_bustrip())->get_next_stop();
+		double stop_position = (next_stop->first)->get_position();
+		double time_to_stop = time + ((exit_time - time) * (stop_position / length)); 
 		// book time to stop
 	}
 // test for type ID and if bus:
