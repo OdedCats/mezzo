8b1d6899db8e156b1e555dde8795b066ea017519
wilco <wilco@be8bedfb-b93a-dc11-ad5b-00609709f61e>
2012-04-14 09:15:32 +0000

    git-svn-id: svn+ssh://ruttval.infra.kth.se/svn/mezzo@335 be8bedfb-b93a-dc11-ad5b-00609709f61e

--- trunk/mezzo_lib/src/parameters.h
+++ trunk/mezzo_lib/src/parameters.h
@@ -50,7 +50,7 @@ extern int vid;     // global vehicle id nr.
 //extern double time_alpha;
 extern std::ofstream eout; // for all debugging output
 
-const string version = "Mezzo 0.60.0";
+const string version = "Mezzo 0.60.1";
 
 // OLD Network.hh parameters
 
